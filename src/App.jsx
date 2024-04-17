be985f8d1f52f9a31f83c1d893f41240f3757fcf
Narenweb <narayanan.sundaram.anywhere.co>
2024-04-17 18:33:08 +0530

    updated vite file

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lets learn Vite</h1>
+      <h1>Lets start with Vite - Naren</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
